522d9de88f4c7e538cbb7cfd66555673adc5fb87
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-08-27 20:01:30 +0300

    Increase stability and reduce cyclomatic complexity

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,16 +1,13 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.filter(passcode=passcode)[0]
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     visits_of_pass = Visit.objects.filter(passcard=passcard)
-    this_passcard_visits = []
-
-    for visit in visits_of_pass:
-        visit_info = collect_visit_info(visit)
-        this_passcard_visits.append(visit_info)
+    this_passcard_visits = [collect_visit_info(visit) for visit in visits_of_pass]
 
     context = {
         'passcard': passcard,
@@ -19,21 +16,13 @@ def passcard_info_view(request, passcode):
     return render(request, 'passcard_info.html', context)
 
 def collect_visit_info(visit):
-    visitor_entered_at = visit.entered_at
-    visit_duration = visit.get_format_duration()
-    visit_is_strange = is_visit_long(visit)
-
     visit_info = {
-            'entered_at': visitor_entered_at,
-            'duration': visit_duration,
-            'is_strange': visit_is_strange
-        }
+            'entered_at': visit.entered_at,
+            'duration': visit.get_formatted_duration(),
+            'is_strange': is_visit_long(visit),
+    }
     return visit_info
 
-def is_visit_long(visit):
-    strange_visit_limit = 3600
+def is_visit_long(visit, strange_visit_limit=3600):
     visit_duration = visit.get_duration().seconds    
-    if visit_duration >= strange_visit_limit:
-        return True
-    else:
-        return False
+    return visit_duration >= strange_visit_limit
