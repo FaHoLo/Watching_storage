60d5416d4b24c879b8878a60db5f47db2140c235
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-08-27 22:49:29 +0300

    Remove debug output

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,7 +1,6 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.db import connection
 
 
 def storage_information_view(request):
@@ -10,15 +9,12 @@ def storage_information_view(request):
     context = {
         'non_closed_visits': non_closed_visits,
     }
-    # print(len(connection.queries))
     return render(request, 'storage_information.html', context)
 
 def collect_non_closed_visit_info(visit):
-    print(len(connection.queries))
     visit_info = {
         'who_entered': visit.passcard.owner_name,
         'entered_at': visit.entered_at,
         'duration': visit.get_formatted_duration(),
     }
-    print(len(connection.queries))
     return visit_info
