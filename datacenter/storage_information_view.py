556b466bbbef850f375fddb5561ebdec67d659c5
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-08-27 20:03:17 +0300

    Improve code readability

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,18 +5,16 @@ from django.shortcuts import render
 
 def storage_information_view(request):
     people_in_storage = Visit.objects.filter(leaved_at=None)  
-    non_closed_visits = []
-
-    for visit in people_in_storage:
-        visitor_name = visit.passcard.owner_name
-        visitor_entered_at = visit.entered_at
-        visit_duration = visit.get_format_duration()
-        non_closed_visits.append({
-            'who_entered': visitor_name,
-            'entered_at': visitor_entered_at,
-            'duration': visit_duration,
-        })
+    non_closed_visits = [collect_non_closed_visit_info(visit) for visit in people_in_storage]
     context = {
         'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
+
+def collect_non_closed_visit_info(visit):
+    visit_info = {
+        'who_entered': visit.passcard.owner_name,
+        'entered_at': visit.entered_at,
+        'duration': visit.get_formatted_duration(),
+    }
+    return visit_info
